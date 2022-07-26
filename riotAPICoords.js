81b495bf4641383fed480f004d909ca27a027773
Deam147 <82825893+Deam147@users.noreply.github.com>
2022-07-25 23:25:18 -0500

    commit

---
+++ riotAPICoords.js
@@ -0,0 +1,333 @@
+var contador = 0
+
+var championTopBlue,championJungleBlue,championMidBlue,championAdcBlue,championSupportBlue;
+var championTopRed,championJungleRed,championMidRed,championAdcRed,championSupportRed;
+
+var playerTopBlue,playerJungleBlue,playerMidBlue,playerAdcBlue,playerSupportBlue;
+var playerTopRed,playerJungleRed,playerMidRed,playerAdcRed,playerSupportRed;
+
+function getNames(){
+    
+
+
+    fetch("./jsonData/ESPORTSTMNT03_2780949data.json")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+
+
+        var summaryData = data
+
+        
+    // Players Blue Side
+    playerTopBlue = summaryData["participants"][0]["summonerName"];
+    playerJungleBlue = summaryData["participants"][1]["summonerName"];
+    playerMidBlue = summaryData["participants"][2]["summonerName"];
+     playerAdcBlue = summaryData["participants"][3]["summonerName"];
+     playerSupportBlue = summaryData["participants"][4]["summonerName"];
+//
+    //Players RedSide
+
+    playerTopRed = summaryData["participants"][5]["summonerName"];
+    playerJungleRed = summaryData["participants"][6]["summonerName"];
+    playerMidRed = summaryData["participants"][7]["summonerName"];
+    playerAdcRed = summaryData["participants"][8]["summonerName"];
+    playerSupportRed = summaryData["participants"][9]["summonerName"];
+
+    // Champions Blue Side
+    championTopBlue = summaryData["participants"][0]["championName"];
+    championJungleBlue = summaryData["participants"][1]["championName"];
+    championMidBlue = summaryData["participants"][2]["championName"];
+    championAdcBlue = summaryData["participants"][3]["championName"];
+    championSupportBlue = summaryData["participants"][4]["championName"];
+
+    //Champions RedSide
+
+    championTopRed = summaryData["participants"][5]["championName"];
+    championJungleRed = summaryData["participants"][6]["championName"];
+    championMidRed = summaryData["participants"][7]["championName"];
+    championAdcRed = summaryData["participants"][8]["championName"];
+    championSupportRed = summaryData["participants"][9]["championName"];
+
+
+    console.log(playerTopBlue)
+    console.log(championTopBlue)
+    console.log(playerTopRed)
+    console.log(championTopRed)
+
+
+
+    })
+
+
+
+  }
+
+function getTimeline(){
+
+  fetch("./jsonData/ESPORTSTMNT03_2780949timeline.json")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+      console.log(data);
+
+      var timelineData = data
+    
+  })
+    
+  return new Promise(function(resolve, reject) {
+    console.log("Envia coordenadas")
+    setTimeout(function() {
+      resolve(timelineData);
+    }, Math.random() * 2000);
+  })
+
+
+
+}
+
+async function getJson() {
+    const response = await fetch('./ESPORTSTMNT03_2780949timeline.json');
+    const data = await response.json();
+    return data;
+}
+
+async function getCoordsAsync(){
+  const cords = await getCoords();
+
+  return cords;
+
+}
+
+
+function getCoords(playerSelect){
+    
+  const coords = [];
+  var victims = [];
+
+    fetch("./jsonData/ESPORTSTMNT03_2780949timeline.json")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+
+
+//printing complete data1
+        var timelineData = data
+
+console.log(timelineData);
+
+
+    console.log("Timeline: " + timelineData["frames"])
+
+
+    for (let i = 2; i < timelineData["frames"].length; i++) {
+        // console.log("frame: "+ i)
+        // console.log("frame length: "+ timelineData["frames"][i]["events"].length)
+
+        for (let y = 0; y < timelineData["frames"][i]["events"].length; y++) {
+
+            //  console.log("event: "+y)
+
+            if (timelineData["frames"][i]["events"][y]["type"] == "CHAMPION_KILL") {
+
+                console.log("found kill de: " + timelineData["frames"][i]["events"][y]["participantId"])
+                victims.push(timelineData["frames"][i]["events"][y]["victimId"])
+
+                if (timelineData["frames"][i]["events"][y]["victimId"] == playerSelect) {
+                    console.log("found death jungle red")
+
+                    coords.push([timelineData["frames"][i]["events"][y]["position"]["x"],timelineData["frames"][i]["events"][y]["position"]["y"],"Champion Kill"])
+
+
+                }
+
+
+            }
+
+
+        }
+
+    }
+    console.log("victims: "+ victims)
+
+    console.log(coords)
+
+    })
+
+   // test(coords)
+   //return coords;
+   return new Promise(function(resolve, reject) {
+    console.log("Envia coordenadas")
+    setTimeout(function() {
+      resolve(coords);
+    }, Math.random() * 2000);
+  })
+                    
+}
+
+
+function wait(ms){
+   var start = new Date().getTime();
+   var end = start;
+   while(end < start + ms) {
+     end = new Date().getTime();
+  }
+}
+
+
+
+async function test(coordenadas) {
+
+
+  const cords = coordenadas
+
+  console.log("test: " +  cords)
+  console.log(cords)
+
+  //Tooltip
+
+
+  // create a tooltip
+  var Tooltip = d3.select("#map")
+    .append("div")
+    .style("opacity", 0)
+    .attr("class", "tooltip").attr("id", contador)
+    .style("background-color", "white")
+    .style("border", "solid")
+    .style("border-width", "2px")
+    .style("border-radius", "5px")
+    .style("padding", "5px").style("position", "absolute")
+
+  // Three function that change the tooltip when user hover / move / leave a cell
+  var mouseover = function(d) {
+    Tooltip
+      .style("opacity", 1)
+    d3.select(this)
+      .style("stroke", "black")
+      .style("opacity", 1)
+  }
+  var mousemove = function(d) {
+    Tooltip
+      .html("The exact value of<br>this cell is: " + d[2])
+      .style("left", (d3.mouse(this)[0]+100) + "px")
+      .style("top", (d3.mouse(this)[1]) + "px")
+  }
+  var mouseleave = function(d) {
+    Tooltip
+      .style("opacity", 0)
+    d3.select(this)
+      .style("stroke", "none")
+      .style("opacity", 0.8)
+  }
+
+
+  //
+
+ var mapCords = cords,
+    // Domain for the current Summoner's Rift on the in-game mini-map
+    domain = {
+      min: { x: -120, y: -120 },
+      max: { x: 14870, y: 14980 },
+    },
+    width = 512,
+    height = 512,
+    bg = "http://ddragon.leagueoflegends.com/cdn/6.8.1/img/map/map11.png",
+    xScale,
+    yScale,
+    svg;
+  const steelblue = d3.rgb("steelblue");
+  color = d3.scale
+    .linear()
+    .domain([0, 3])
+    .range(["red", "red"])
+    .interpolate(d3.interpolateLab);
+
+  xScale = d3.scale
+    .linear()
+    .domain([domain.min.x, domain.max.x])
+    .range([0, width]);
+
+  yScale = d3.scale
+    .linear()
+    .domain([domain.min.y, domain.max.y])
+    .range([height, 0]);
+
+  svg = d3
+    .select("#map")
+    .append("svg:svg")
+    .attr("width", width)
+    .attr("height", height).attr("class", "centerImg").attr("id", contador);
+
+  svg
+    .append("image")
+    .attr("xlink:href", bg)
+    .attr("x", "0")
+    .attr("y", "0")
+    .attr("width", width)
+    .attr("height", height);
+
+  svg
+    .append("svg:g")
+    .selectAll("circle")
+    .data(cords)
+    .enter()
+    .append("svg:circle")
+    .attr("cx", function (d) {
+      return xScale(d[0]);
+    })
+    .attr("cy", function (d) {
+      return yScale(d[1]);
+    })
+    .attr("r", 5)
+    .attr("class", "kills")
+    .style("fill", "#FF0000").on("mouseover", mouseover)
+    .on("mousemove", mousemove)
+    .on("mouseleave", mouseleave);
+
+
+    console.log("Coordenadas en mapa: " + cords)
+//console.log("Coordenadas de timeline: " + getCoordsAsync())
+
+}
+
+async function run(){
+
+  getNames();
+  if(contador >= 2){
+    deleteMap(contador-1)
+
+
+  }
+
+
+contador= contador+1
+
+  var select = document.getElementById('selectPlayer');
+var value = select.options[select.selectedIndex].value;
+
+console.log("player select: " + value)
+  console.log('Before promise call.')
+
+  const cords = await getCoords(value);
+
+ console.log("Run cords: "+cords)
+ console.log(cords)
+
+   test(cords);
+}
+
+function deleteMap(id){
+  var map = document.getElementById(id);
+  if(map !== null){
+
+    map.remove();
+
+  }
+
+}
+
